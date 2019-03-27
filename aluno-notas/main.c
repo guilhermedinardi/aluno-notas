e0b8908364cdd5f8773294f178e5325cbd47bc9b
Guilherme Dinardi <guilhermedinardi@macbook-pro-de-guilherme.local>
2019-03-27 18:39:02 -0300

    Initial Commit

---
+++ aluno-notas/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  aluno-notas
+//
+//  Created by Guilherme Dinardi on 27/03/19.
+//  Copyright © 2019 Guilherme Dinardi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
