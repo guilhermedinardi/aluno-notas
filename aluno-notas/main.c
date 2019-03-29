241aace897cfc5008e0960769394407095f77e18
Guilherme Dinardi <guilhermedinardi@macbook-pro-de-guilherme.local>
2019-03-29 18:48:55 -0300

    projeto algoritmo

--- aluno-notas/main.c
+++ aluno-notas/main.c
@@ -7,9 +7,44 @@
 //
 
 #include <stdio.h>
-
+#include <string.h>
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    // Declaração de Variaves
+    float nota1, nota2, nota3, media;
+    char  nome;
+    
+    // Entrada de Dados
+    printf("\n Digite seu nome: ");
+    scanf("%c",&nome);
+    
+    printf("\n Digite a sua primeira nota: ");
+    scanf("%f", &nota1);
+    
+    printf("\n Digite a sua segunda nota: ");
+    scanf("%f", &nota2);
+    
+    printf("\n Digite a sua terceira nota: ");
+    scanf("%f", &nota3);
+    
+    // Declaração de Condicionais
+    if ((nota1>nota2 ) && (nota1 > nota3)) {
+        media = (nota1 * 3 + (nota2 * 2) + (nota3 * 2)) /7;
+    } else {
+        if ((nota2 > nota1) && (nota2 > nota3)) {
+            media = ((nota1 * 2) + (nota2 * 3) + (nota3 * 2)) /7;
+        } else {
+            media = ((nota1 * 2) + (nota2 * 2) + (nota3 * 3)) /7;
+        }
+    }
+    // Saida de Dados
+    printf("Nome: %c", nome);
+    
+    // Declaração de Condicionais media
+    if (media >= 7.0) {
+        printf("\n APROVADO!");
+    } else{
+        printf("\n REPROVADO! \n");
+    }
+    printf("\n");
     return 0;
 }
